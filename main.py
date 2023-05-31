64c7c295c60a884ba06e0c73ec31fd0debcc09ce
apeng-singlestore <127370261+apeng-singlestore@users.noreply.github.com>
2023-05-31 15:46:33 -0700

    added credential input in chat, credentials in env and simple error handling

--- main.py
+++ main.py
@@ -3,70 +3,70 @@ import quart
 import quart_cors
 from quart import request
 import requests
+import dotenv
+import os
 
+    
+if not dotenv.load_dotenv():
+    with open('.env', 'w') as f:
+        f.write('USERNAME=\n')
+        f.write('PASSWORD=\n')
+        f.write('ENDPOINT_URL=\n')
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
-
-
-# Keep track of username and password
-username = 'admin'
-password = 'bIM2J78ZKZDeDetiwzLifEkfddtNA8nK'
-endpoint_url = 'svc-bdaf1a6b-098e-47a4-9a84-c6ff70b0d6b2-dml.aws-virginia-6.svc.singlestore.com'
-
 # Add the new API endpoint
 @app.post("/sql_query")
 async def execute_sql_query():
     request_data = await quart.request.get_json(force=True)
     sql_query = request_data["sql_query"]
     db_name = request_data["db_name"]
-    result = post_sql_query(sql_query, db_name, username, password)
+    result = post_sql_query(sql_query, db_name, os.getenv('USERNAME'), os.getenv('PASSWORD'))
     return quart.Response(response=json.dumps(result), status=200)
 
 def post_sql_query(sql_query, db_name, username, password):
-    url = "https://" + endpoint_url + "/api/v2/query/rows"
-    
-    # Add the authorization header
-    headers = {
-        "Content-Type": "application/json"
-    }
-    
-    data = {"sql": sql_query, "database": db_name}
+    print("CREDENTIALS")
+    print(username)
+    print(password)
 
-    
-    response = requests.post(url, headers=headers, data=json.dumps(data), auth=(username, password))
-    print(response)
-    return response.json()
+    try:
+        url = "https://" + os.getenv('ENDPOINT_URL') + "/api/v2/query/rows"
+        
+        # Add the authorization header
+        headers = {
+            "Content-Type": "application/json"
+        }
+        
+        data = {"sql": sql_query, "database": db_name}
+
+        
+        response = requests.post(url, headers=headers, data=json.dumps(data), auth=(username, password))
+        print(response)
+        return response.json()
+    except:
+        return quart.Response(response="SQL Query error. Please check your credentials.", status=400)
 
     
 @app.get("/table_shape")
 async def get_table_shape():
     query = "SELECT table_schema, table_name, GROUP_CONCAT(column_name SEPARATOR ', ') AS columns FROM information_schema.columns WHERE table_schema != 'information_schema' GROUP BY table_schema, table_name ORDER BY table_schema;"
-    result = post_sql_query(query, "information_schema", username, password)
-    return quart.Response(response=json.dumps(result), status=200)
+    try:
+        result = post_sql_query(query, "information_schema", os.getenv('USERNAME'), os.getenv('PASSWORD'))
+        return quart.Response(response=json.dumps(result), status=200)
+    except:
+        return quart.Response(response="SQL Query error. Please check your credentials.", status=400)
+
+        
+    
 
 
-
-
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
-
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
-
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+@app.post("/credentials")
+async def save_credentials():
+    request_data = await quart.request.get_json(force=True)
+    dotenv.set_key(dotenv_path=dotenv.find_dotenv(), key_to_set='USERNAME', value_to_set=request_data['username'])
+    dotenv.set_key(dotenv_path=dotenv.find_dotenv(), key_to_set='PASSWORD', value_to_set=request_data['password'])
+    dotenv.set_key(dotenv_path=dotenv.find_dotenv(), key_to_set='ENDPOINT_URL', value_to_set=request_data['endpoint_url'])
+    dotenv.load_dotenv(override=True)
+    return quart.Response(response="OK", status=200)
 
 @app.get("/logo.png")
 async def plugin_logo():
